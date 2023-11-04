45dc812c1b578aa35c4e63ef56839dde626fffa7
enzo-hmdy <enzo.hmdy@gmail.com>
2023-11-04 18:09:05 -0400

    comit

--- array_to_graph.py
+++ array_to_graph.py
@@ -1,40 +1,48 @@
 def array_to_weighted_graph(array):
     graph = {}
-    rows = len(array)
-    cols = len(array[0])
+    current_letter = ['A']
 
-    # Find the coordinates of 1, 2, and 4
-    start_coords = None
-    end_coords = []
+    def generate_elem(coords, is_start, is_end):
+        graph[current_letter[0]] = {
+            'letter': current_letter[0],
+            'coords': coords,
+            'isStart': is_start,
+            'isEnd': is_end,
+            'dest': {}
+        }
+        current_letter[0] = chr(ord(current_letter[0]) + 1)
 
-    for i in range(rows):
-        for j in range(cols):
-            value = array[i][j]
-            coords = (i, j)
-            if value == 1:
-                start_coords = (coords, value)
-            elif value == 2 or value == 4:
-                end_coords.append((coords, value))
+    def add_dest(from_, to, distance):
+        graph[from_]['dest'][to] = distance
 
-    if not start_coords or not end_coords:
-        return graph  # No start or end nodes found
+    for y, sub in enumerate(array):
+        for x, elem in enumerate(sub):
+            if elem == 1:
+                generate_elem([x, y], True, False)
+            elif elem == 2:
+                generate_elem([x, y], False, False)
+            elif elem == 4:
+                generate_elem([x, y], False, True)
 
-    # Calculate distances between 1 and 2/4 and between 2/4
-    for (end_coord, end_value) in end_coords:
-        distance = abs(start_coords[0][0] - end_coord[0]) + abs(start_coords[0][1] - end_coord[1])
-        graph[(start_coords, (end_coord, end_value))] = distance
+    point_list = list(graph.keys())
 
-    for i in range(len(end_coords)):
-        for j in range(i + 1, len(end_coords)):
-            distance = abs(end_coords[i][0][0] - end_coords[j][0][0]) + abs(end_coords[i][0][1] - end_coords[j][0][1])
-            graph[(end_coords[i], end_coords[i][1]), (end_coords[j], end_coords[j][1])] = distance
+    for current_letter[0] in point_list:
+        current_elem = graph[current_letter[0]]
+        other_letters = [l for l in point_list if l != current_letter[0]]
+
+        for l in other_letters:
+            e = graph[l]
+            dist = abs(current_elem['coords'][0] - e['coords'][0]) + abs(current_elem['coords'][1] - e['coords'][1])
+            add_dest(current_letter[0], l, dist)
 
     return graph
 
-my_map = [[1, 0, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2], [0, 0, 0, 0, 0], [4, 0, 0, 2, 0]]
-weighted_graph = array_to_weighted_graph(my_map)
 
-# Printing the weighted graph with integer distances
-for (node1, value1), (node2, value2) in weighted_graph.keys():
-    distance = weighted_graph[(node1, value1), (node2, value2)]
-    print(f"Distance from Node {node1} (Value {value1}) to Node {node2} (Value {value2}): {int(distance)}")
+map = [[1,0,0,0,0],
+       [0,2,0,0,0],
+       [0,0,0,2,0],
+       [0,2,0,0,0],
+       [0,0,0,0,4]]
+
+graph = array_to_weighted_graph(map)
+print(graph)
