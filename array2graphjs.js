9dcd0387fe8261d1012944e581d66a73046ff5fd
enzo-hmdy <enzo.hmdy@gmail.com>
2023-11-04 11:35:30 -0400

    updated

--- array2graphjs.js
+++ array2graphjs.js
@@ -1,36 +1,48 @@
-function array_to_graph(array) {
-    let graph = {};
-    let rows = array.length;
-    let cols = array[0].length;
-    let neighbors = [[0, 1], [0, -1], [1, 0], [-1, 0]];
+function array_to_weighted_graph(array) {
+    const graph = {};
+    const rows = array.length;
+    const cols = array[0].length;
+
+    // Find the coordinates of 1, 2, and 4
+    let start_coords = null;
+    const end_coords = [];
 
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
-            let node_value = array[i][j];
-            let node = [i, j];
-            let neighbors_list = [];
-
-            for (let [dx, dy] of neighbors) {
-                let x = i + dx;
-                let y = j + dy;
-
-                if (0 <= x && x < rows && 0 <= y && y < cols) {
-                    let neighbor_value = array[x][y];
-                    let neighbor = [x, y];
-                    neighbors_list.push([neighbor, neighbor_value]);
-                }
+            const value = array[i][j];
+            const coords = [i, j];
+            if (value === 1) {
+                start_coords = [coords, value];
+            } else if (value === 2 || value === 4) {
+                end_coords.push([coords, value]);
             }
+        }
+    }
 
-            graph[[node, node_value]] = neighbors_list;
+    if (!start_coords || !end_coords) {
+        return graph;  // No start or end nodes found
+    }
+
+    // Calculate distances between 1 and 2/4 and between 2/4
+    for (const [end_coord, end_value] of end_coords) {
+        const distance = Math.abs(start_coords[0][0] - end_coord[0]) + Math.abs(start_coords[0][1] - end_coord[1]);
+        graph[[start_coords, [end_coord, end_value]]] = distance;
+    }
+
+    for (let i = 0; i < end_coords.length; i++) {
+        for (let j = i + 1; j < end_coords.length; j++) {
+            const distance = Math.abs(end_coords[i][0][0] - end_coords[j][0][0]) + Math.abs(end_coords[i][0][1] - end_coords[j][0][1]);
+            graph[[end_coords[i], end_coords[i][1]], [end_coords[j], end_coords[j][1]]] = distance;
         }
     }
 
     return graph;
 }
 
-let my_map = [[1, 0, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2], [0, 0, 0, 0, 0], [4, 0, 0, 2, 0]];
-let graph = array_to_graph(my_map);
+const my_map = [[1, 0, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2], [0, 0, 0, 0, 0], [4, 0, 0, 2, 0]];
+const weighted_graph = array_to_weighted_graph(my_map);
 
-for (let [[x, y], node_value] of Object.keys(graph)) {
-    console.log(`Node (${x}, ${y}), value ${node_value} connects to: ${graph[[x, y], node_value]}`);
+for (const [[node1, value1], [node2, value2]] of Object.keys(weighted_graph)) {
+    const distance = weighted_graph[[node1, value1], [node2, value2]];
+    console.log(`Distance from Node ${node1} (Value ${value1}) to Node ${node2} (Value ${value2}): ${Math.floor(distance)}`);
 }
