b841e7239e16ea96113b4c3f5041f296e38f8389
Zalax <contact@zalax.xyz>
2023-11-04 12:10:09 -0400

    feat: try a new approach and algorithm

--- array2graphjs.js
+++ array2graphjs.js
@@ -1,48 +1,74 @@
 function array_to_weighted_graph(array) {
-    const graph = {};
-    const rows = array.length;
-    const cols = array[0].length;
+    const graph = {
 
-    // Find the coordinates of 1, 2, and 4
-    let start_coords = null;
-    const end_coords = [];
+    };
 
-    for (let i = 0; i < rows; i++) {
-        for (let j = 0; j < cols; j++) {
-            const value = array[i][j];
-            const coords = [i, j];
-            if (value === 1) {
-                start_coords = [coords, value];
-            } else if (value === 2 || value === 4) {
-                end_coords.push([coords, value]);
+
+    let currentLetter = "A"
+    function generateElem(coords, isStart, isValve, isEnd) {
+        graph[currentLetter] = {
+            "coords": coords,
+            "isStart": isStart,
+            "isValve": isValve,
+            "isEnd": isEnd,
+            "dest": {
+
             }
         }
+
+        currentLetter = String.fromCharCode(currentLetter.charCodeAt() + 1)
     }
 
-    if (!start_coords || !end_coords) {
-        return graph;  // No start or end nodes found
+    function addDest(from, to, distance) {
+        graph[from]["dest"][to] = distance
     }
 
-    // Calculate distances between 1 and 2/4 and between 2/4
-    for (const [end_coord, end_value] of end_coords) {
-        const distance = Math.abs(start_coords[0][0] - end_coord[0]) + Math.abs(start_coords[0][1] - end_coord[1]);
-        graph[[start_coords, [end_coord, end_value]]] = distance;
+    array.forEach((sub, y) => {
+        console.log(sub)
+        sub.forEach((elem, x) => {
+            switch (elem) {
+                case 1:
+                    generateElem([x, y], true, false, false)
+                    break;
+                case 2:
+                    generateElem([x, y], false, true, false)
+                    break;
+                case 4:
+                    generateElem([x, y], false, false, true)
+                    break;
+                case 0:
+                default:
+                    null;
+            }
+        })
+    })
+
+
+
+    // We now have a nice list of every point, we just need to add neighboors.
+
+
+    const pointList = [];
+    for (letter in graph) {
+        pointList.push(letter)
     }
 
-    for (let i = 0; i < end_coords.length; i++) {
-        for (let j = i + 1; j < end_coords.length; j++) {
-            const distance = Math.abs(end_coords[i][0][0] - end_coords[j][0][0]) + Math.abs(end_coords[i][0][1] - end_coords[j][0][1]);
-            graph[[end_coords[i], end_coords[i][1]], [end_coords[j], end_coords[j][1]]] = distance;
-        }
-    }
+
+    // On repasse sur chaque point.
+    pointList.forEach(currentLetter => {
+        const currentElem = graph[currentLetter];
+        const otherLetters = pointList.filter(e => e !== currentLetter);
+
+        otherLetters.forEach(l => {
+            console.log("currentLetter: ", currentLetter, "  destination: ", l)
+            const e = graph[l];
+
+            const dist = Math.abs(currentElem["coords"][0] - e["coords"][0]) + Math.abs(currentElem["coords"][1] - e["coords"][1])
+            addDest(currentLetter, l, dist);
+        })
+    })
 
     return graph;
 }
 
-const my_map = [[1, 0, 2, 0, 0], [0, 0, 0, 0, 0], [0, 0, 0, 0, 2], [0, 0, 0, 0, 0], [4, 0, 0, 2, 0]];
-const weighted_graph = array_to_weighted_graph(my_map);
-
-for (const [[node1, value1], [node2, value2]] of Object.keys(weighted_graph)) {
-    const distance = weighted_graph[[node1, value1], [node2, value2]];
-    console.log(`Distance from Node ${node1} (Value ${value1}) to Node ${node2} (Value ${value2}): ${Math.floor(distance)}`);
-}
+module.exports = array_to_weighted_graph
--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000', {reconnect: true});
 
+const array_to_graph = require("./array2graphjs");
+
+
 // Add a connect listener
 socket.on('connect', function (socket) {
     console.log('Connected!');
@@ -13,6 +16,8 @@ socket.emit("newgame", ["lvl0", 2], newGameResponse => {
     console.log(newGameResponse)
 
     socket.emit("gamestatus", gameId, gameStatusResponse => {
+        gameStatusResponse = JSON.parse(gameStatusResponse)
         console.log(gameStatusResponse)
+        console.log(array_to_graph(gameStatusResponse))
     })
 })
